02df746e89ac8e8d09a8401e87f9c63e9fedf741
HaidarRais <zwingfame@gmail.com>
2020-10-07 00:13:50 +0700

    Add create feature

---
+++ create_siswa.php
@@ -0,0 +1,29 @@
+<?php
+	include "connect.php";
+	$con = mysqli_connect (HOST, USER, PASSWORD, DATABASE);
+	$response = array();
+
+	$nama_siswa = $_POST ["nama_siswa"];
+	$kelas = $_POST ["kelas"];
+
+	$sql = "INSERT INTO tb_siswa(id_siswa, nama_siswa, kelas) 
+	VALUES(UUID(), '$nama_siswa', '$kelas')";
+
+	$result = mysqli_query($con, $sql);
+
+	if ($result) 
+	{
+		$resp["status"] = "1";
+		$resp["message"] = "sukses";
+		$resp["nama_guru"] = $nama_siswa;
+		$resp["kelas"] = $kelas;
+	}else
+	{
+		$resp["status"] = "0";
+		$resp["message"] = "gagal";
+	}
+	$response = $resp;
+	echo json_encode ($response);
+
+	mysqli_close ($con);
+?>
