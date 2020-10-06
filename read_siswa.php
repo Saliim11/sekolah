043588e8875fda011365b59bfa349a4d1902ce7d
HaidarRais <zwingfame@gmail.com>
2020-10-07 00:23:05 +0700

    Add read and update on siswa

---
+++ read_siswa.php
@@ -0,0 +1,16 @@
+<?php
+include "connect.php";
+$con = mysqli_connect (HOST, USER, PASSWORD, DATABASE);
+
+$response = array();
+
+$sql = "SELECT * FROM `tb_siswa`";
+
+$result = mysqli_query($con, $sql);
+
+foreach($result as $key => $value){
+	array_push($response, $value);
+}
+
+print(json_encode($response));
+?>
