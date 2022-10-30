75bc50452e0d1078f5b876bc0e390635a4ba9032
HaidarRais <zwingfame@gmail.com>
2022-10-30 22:27:23 +0700

    update delete_siswa.php
    comment for readable

--- delete_siswa.php
+++ delete_siswa.php
@@ -3,13 +3,13 @@ include "connect.php";
 $con = mysqli_connect (HOST, USER, PASSWORD, DATABASE);
 
 $id = $_GET ['id_siswa'];
-
+//query
 $sql = "DELETE from tb_siswa WHERE id_siswa = '$id'";
 
 $result = mysqli_query($con,$sql);
 
 $response = array();
-
+// else if condition
 if($result) {
 	$resp["status"] = 1;
 	$resp["message"] = "hapus sukses";
