fcbf53b50333537ca2caeb33194413f7a210ece4
Reva Doni Aprilio <aprilio842@gmail.com>
2022-10-30 19:57:37 +0700

    Update login_user.php

--- login_user.php
+++ login_user.php
@@ -1,15 +1,16 @@
 <?php
+	// Untuk menghubungkan ke database
 	include "connect.php";
 	$con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 	
 	$response = array ();
 	
+	// menampung nilai username dan password dari form
 	$username = $_POST['vsusername'];
 	$password = md5($_POST['vspassword']);
-	//variables
-	$sql = "SELECT * FROM users WHERE vsusername = '$username' and vspassword = '$password'";
-	//query
 	
+	// query cek username dan password
+	$sql = "SELECT * FROM users WHERE vsusername = '$username' and vspassword = '$password'";
 	$result = mysqli_query($con, $sql);
 	
 	if (!$result) {
